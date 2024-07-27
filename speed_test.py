d923cda22bbed18ab81bc042b0f2eab103025afd
Sam Leonard <samuelleonard35@gmail.com>
2024-07-27 13:38:10 +0100

    Added Sponge{128,256} and Shaker{128,256}

--- speed_test.py
+++ speed_test.py
@@ -1,6 +1,11 @@
 import random
 from hashlib import shake_128
-from xof_py import pyo3_shake_128, pyo3_shake_128_one_block, pyo3_shake_128_n_blocks, Shake128_pyo3
+from xof_py import (
+    pyo3_shake_128,
+    pyo3_shake_128_one_block,
+    pyo3_shake_128_n_blocks,
+    Shake128_pyo3,
+)
 import time
 import os
 
@@ -19,18 +24,18 @@ for _ in range(100):
     b = pyo3_shake_128(absorb, n)
     assert a == b
 
-for _ in range(100):
-    absorb = os.urandom(32)
-    a = shake_128(absorb).digest(168)
-    b = pyo3_shake_128_one_block(absorb)
-    assert a == b
+# for _ in range(100):
+#     absorb = os.urandom(32)
+#     n = random.randint(1, 1000)
+#     a = shake_128(absorb).digest(n)
+#     b = pyo3_shake_128(absorb, n)
+#     assert a == b
 
-for _ in range(100):
-    absorb = os.urandom(32)
-    n = random.randint(1, 32)
-    a = shake_128(absorb).digest(168 * n)
-    b = pyo3_shake_128_n_blocks(absorb, n)
-    assert a == b
+# for _ in range(100):
+#     absorb = os.urandom(32)
+#     a = shake_128(absorb).digest(168)
+#     b = pyo3_shake_128_one_block(absorb)
+#     assert a == b
 
 random.seed(0)
 t0 = time.time()
@@ -47,53 +52,4 @@ for _ in range(10_000):
     a = pyo3_shake_128(b"123", n)
 print(f"10_000 calls with rust sha3: {time.time() - t0 }")
 
-random.seed(0)
-t0 = time.time()
-for _ in range(10_000):
-    n = random.randint(1, 5000)
-    a = shake_128(b"123").digest(n)
-print(f"10_000 calls with hashlib: {time.time() - t0}")
-
-print("-"*80)
-
-random.seed(0)
-t0 = time.time()
-for _ in range(10_000):
-    a = pyo3_shake_128_one_block(b"123")
-print(f"10_000 one block calls with rust sha3: {time.time() - t0 }")
-
-random.seed(0)
-t0 = time.time()
-for _ in range(10_000):
-    a = shake_128(b"123").digest(168)
-print(f"10_000 one block calls with hashlib: {time.time() - t0}")
-
-print()
-
-random.seed(0)
-t0 = time.time()
-for _ in range(10_000):
-    a = pyo3_shake_128_n_blocks(b"123", 2)
-print(f"10_000 2 block calls with rust sha3: {time.time() - t0 }")
-
-random.seed(0)
-t0 = time.time()
-for _ in range(10_000):
-    a = shake_128(b"123").digest(168 * 2)
-print(f"10_000 2 block calls with hashlib: {time.time() - t0}")
-
-print()
-
-random.seed(0)
-t0 = time.time()
-for _ in range(10_000):
-    n = random.randint(1, 32)
-    a = pyo3_shake_128_n_blocks(b"123", n)
-print(f"10_000 n block calls with rust sha3: {time.time() - t0 }")
-
-random.seed(0)
-t0 = time.time()
-for _ in range(10_000):
-    n = random.randint(1, 32)
-    a = shake_128(b"123").digest(168 * n)
-print(f"10_000 n block calls with hashlib: {time.time() - t0}")
+print("-" * 80)
--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,10 @@
-use pyo3::prelude::*;
-use pyo3::types::PyBytes;
+use pyo3::{prelude::*, types::PyBytes};
 use sha3::{
     digest::{
-        core_api::XofReaderCoreWrapper, crypto_common::BlockSizeUser, ExtendableOutput, Update,
-        XofReader,
+        core_api::XofReaderCoreWrapper, crypto_common::BlockSizeUser, ExtendableOutput,
+        ExtendableOutputReset, Update, XofReader,
     },
-    Shake128, Shake128ReaderCore,
+    Shake128, Shake128ReaderCore, Shake256, Shake256ReaderCore,
 };
 
 // Very silly first attempt at a class
@@ -68,12 +67,85 @@ fn pyo3_shake_128_n_blocks<'py>(
     pyo3_shake_128(py, input_bytes, n * Shake128::block_size())
 }
 
+macro_rules! impl_sponge_shaker_classes {
+    ($hasher:ident, $xof_reader:ident, $shaker_name:ident, $sponge_name:ident) => {
+        #[pyclass]
+        struct $sponge_name {
+            xof: XofReaderCoreWrapper<$xof_reader>,
+        }
+
+        #[pymethods]
+        impl $sponge_name {
+            fn read<'py>(&mut self, py: Python<'py>, n: usize) -> PyResult<Bound<'py, PyBytes>> {
+                PyBytes::new_bound_with(py, n, |bytes| {
+                    self.xof.read(bytes);
+                    Ok(())
+                })
+            }
+
+            fn __repr__(&self) -> String {
+                String::from(stringify!($sponge_name))
+            }
+
+            fn __str__(&self) -> String {
+                self.__repr__()
+            }
+        }
+
+        #[pyclass]
+        struct $shaker_name {
+            hasher: $hasher,
+        }
+
+        #[pymethods]
+        impl $shaker_name {
+            #[new]
+            #[pyo3(signature = (input_bytes = None))]
+            fn new(input_bytes: Option<&[u8]>) -> Self {
+                let mut hasher = $hasher::default();
+                if let Some(initial_data) = input_bytes {
+                    hasher.update(initial_data);
+                }
+                Self { hasher }
+            }
+
+            fn absorb(&mut self, input_bytes: &[u8]) {
+                self.hasher.update(input_bytes);
+            }
+
+            fn finalize(&mut self) -> $sponge_name {
+                $sponge_name {
+                    xof: self.hasher.finalize_xof_reset(),
+                }
+            }
+
+            fn __repr__(&self) -> String {
+                String::from(stringify!($shaker_name))
+            }
+
+            fn __str__(&self) -> String {
+                self.__repr__()
+            }
+        }
+    };
+}
+
+impl_sponge_shaker_classes!(Shake128, Shake128ReaderCore, Shaker128, Sponge128);
+impl_sponge_shaker_classes!(Shake256, Shake256ReaderCore, Shaker256, Sponge256);
+
 /// A Python module implemented in Rust.
 #[pymodule]
 fn xof_py(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(pyo3_shake_128, m)?)?;
     m.add_function(wrap_pyfunction!(pyo3_shake_128_one_block, m)?)?;
     m.add_function(wrap_pyfunction!(pyo3_shake_128_n_blocks, m)?)?;
+
     m.add_class::<Shake128Py>()?;
+
+    m.add_class::<Sponge128>()?;
+    m.add_class::<Shaker128>()?;
+    m.add_class::<Sponge256>()?;
+    m.add_class::<Shaker256>()?;
+
     Ok(())
 }
