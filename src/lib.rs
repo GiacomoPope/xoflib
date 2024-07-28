b1e487077bbfdd716d4e8e7e29016181034d257a
Robin Jadoul <robin.jadoul@gmail.com>
2024-07-28 18:41:56 +0200

    Different macro attempt

--- src/lib.rs
+++ src/lib.rs
@@ -1,33 +1,70 @@
 use pyo3::{exceptions::PyValueError, prelude::*, types::PyBytes};
 use sha3::{
-    digest::{consts::*, core_api::*, typenum::*, *},
-    Shake128, Shake128ReaderCore, Shake256, Shake256ReaderCore, TurboShake128, TurboShake128Core,
-    TurboShake128ReaderCore, TurboShake256, TurboShake256Core, TurboShake256ReaderCore,
+    digest::*, Shake128, Shake256, TurboShake128, TurboShake128Core, TurboShake256,
+    TurboShake256Core,
 };
 
-macro_rules! impl_sponge_shaker_classes {
-    // hasher is tt so we can pick the right kind of methods to generate
-    ($hasher:tt, $xof_reader:ident, $shaker_name:ident, $sponge_name:ident) => {
+macro_rules! impl_shaker_and_sponge {
+    ($shaker_name:ident, $sponge_name:ident, $inner:ty, default) => {
+        impl_shaker_and_sponge!(@impl $shaker_name, $sponge_name, $inner, (), (), (Ok::<_, PyErr>(Default::default())));
+    };
+
+    ($shaker_name:ident, $sponge_name:ident, $inner:ty, $($argname:ident: $argty:ty),* => $create:ident $(| $($extra:tt)*)?) => {
+        impl_shaker_and_sponge!(@impl $shaker_name,
+                                      $sponge_name,
+                                      $inner,
+                                      ($($argname),*, ),
+                                      ($($argname: $argty),*, ),
+                                      ($create!($inner, $($argname),* $(| $($extra)*)?)));
+    };
+
+    (@impl $shaker_name:ident,
+           $sponge_name:ident,
+           $inner:ty,
+           ($($new_sig:tt)*),
+           ($($shaker_ctor_args:tt)*),
+           ($($shaker_ctor:tt)*)) => {
         #[pyclass]
-        struct $shaker_name {
-            hasher: $hasher,
+        struct $shaker_name(Shaker<$inner>);
+
+        #[pymethods]
+        impl $shaker_name {
+            #[new]
+            #[pyo3(signature = ($($new_sig)* input_bytes = None))]
+            fn new($($shaker_ctor_args)* input_bytes: Option<&[u8]>) -> PyResult<Self> {
+                let mut res = Self($($shaker_ctor)*?);
+                if let Some(input) = input_bytes {
+                    res.absorb(input)?;
+                }
+                Ok(res)
+            }
+
+            fn absorb(&mut self, input_bytes: &[u8]) -> PyResult<()> {
+                self.0.absorb(input_bytes)
+            }
+
+            fn finalize(&mut self) -> PyResult<$sponge_name> {
+                Ok($sponge_name(self.0.finalize()?))
+            }
         }
 
-        impl_sponge_shaker_classes!(@shaker_methods $hasher, $shaker_name, $sponge_name);
-
         #[pyclass]
-        struct $sponge_name {
-            xof: XofReaderCoreWrapper<$xof_reader>,
-        }
+        struct $sponge_name(Sponge<$inner>);
 
         #[pymethods]
         impl $sponge_name {
             fn read<'py>(&mut self, py: Python<'py>, n: usize) -> PyResult<Bound<'py, PyBytes>> {
-                PyBytes::new_bound_with(py, n, |bytes| {
-                    self.xof.read(bytes);
-                    Ok(())
-                })
+                PyBytes::new_bound_with(py, n, |buffer| self.0.read_into(buffer))
             }
+
+            // Can't really do this cleanly without unsafe :/
+            // fn read_into<'py>(
+            //     &mut self,
+            //     py: Python<'py>,
+            //     buffer: Bound<'py, PyByteArray>,
+            // ) -> PyResult<()> {
+            //     self.0.read_into(buffer)
+            // }
 
             fn __repr__(&self) -> String {
                 String::from(stringify!($sponge_name))
@@ -38,155 +75,62 @@ macro_rules! impl_sponge_shaker_classes {
             }
         }
     };
-
-    // "match" on the TurboShakes and generate a unique __init__ for them with domain separation
-    (@shaker_methods TurboShake128, $shaker_name:ident, $sponge_name:ident) => {
-        impl_sponge_shaker_classes!(@turbo_shaker_methods TurboShake128Core, $shaker_name, $sponge_name);
-    };
-
-    (@shaker_methods TurboShake256, $shaker_name:ident, $sponge_name:ident) => {
-        impl_sponge_shaker_classes!(@turbo_shaker_methods TurboShake256Core, $shaker_name, $sponge_name);
-    };
-
-    (@turbo_shaker_methods $hasher_core:ident, $shaker_name:ident, $sponge_name:ident) => {
-        #[pymethods]
-        impl $shaker_name {
-            #[new]
-            #[pyo3(signature = (domain_sep, input_bytes = None))]
-            fn new(domain_sep: u8, input_bytes: Option<&[u8]>) -> PyResult<Self> {
-                if !(0x01..=0x7F).contains(&domain_sep) {
-                    return Err(PyValueError::new_err("domain sep is not in range(1, 0x80)"))
-                }
-
-                let mut hasher = CoreWrapper::from_core($hasher_core::new(domain_sep));
-                if let Some(initial_data) = input_bytes {
-                    hasher.update(initial_data);
-                }
-
-                Ok(Self { hasher })
-            }
-
-            fn absorb(&mut self, input_bytes: &[u8]) {
-                self.hasher.update(input_bytes);
-            }
-
-            fn finalize(&mut self) -> $sponge_name {
-                $sponge_name {
-                    xof: self.hasher.finalize_xof_reset(),
-                }
-            }
-
-            fn __repr__(&self) -> String {
-                String::from(stringify!($shaker_name))
-            }
-
-            fn __str__(&self) -> String {
-                self.__repr__()
-            }
-        }
-    };
-
-    // I would love to be more specific with the template but annoyingly you cannot use a macro in
-    // a #[pymethods] block to define methods :(
-    (@shaker_methods $hasher:ident, $shaker_name:ident, $sponge_name:ident) => {
-        #[pymethods]
-        impl $shaker_name {
-            #[new]
-            #[pyo3(signature = (input_bytes = None))]
-            fn new(input_bytes: Option<&[u8]>) -> Self {
-                let mut hasher = $hasher::default();
-                if let Some(initial_data) = input_bytes {
-                    hasher.update(initial_data);
-                }
-                Self { hasher }
-            }
-
-            fn absorb(&mut self, input_bytes: &[u8]) {
-                self.hasher.update(input_bytes);
-            }
-
-            fn finalize(&mut self) -> $sponge_name {
-                $sponge_name {
-                    xof: self.hasher.finalize_xof_reset(),
-                }
-            }
-
-            fn __repr__(&self) -> String {
-                String::from(stringify!($shaker_name))
-            }
-
-            fn __str__(&self) -> String {
-                self.__repr__()
-            }
-        }
-    };
 }
 
-// impl_sponge_shaker_classes!(Shake128, Shake128ReaderCore, Shaker128, Sponge128);
-impl_sponge_shaker_classes!(Shake256, Shake256ReaderCore, Shaker256, Sponge256);
-impl_sponge_shaker_classes!(
-    TurboShake128,
-    TurboShake128ReaderCore,
-    TurboShaker128,
-    TurboSponge128
-);
-impl_sponge_shaker_classes!(
-    TurboShake256,
-    TurboShake256ReaderCore,
-    TurboShaker256,
-    TurboSponge256
-);
-
-struct Shaker<H>
-where
-    H: BufferKindUser,
-    H::BlockSize: IsLess<U256>,
-    Le<H::BlockSize, U256>: NonZero,
-{
-    hasher: CoreWrapper<H>,
-}
+#[derive(Default)]
+struct Shaker<H>(H);
 
 impl<H> Shaker<H>
 where
-    H: BufferKindUser + Default + UpdateCore,
-    CoreWrapper<H>: Copy + Clone + ExtendableOutputReset,
-    H::BlockSize: Default,
-    H::BufferKind: Default,
-    H::BlockSize: IsLess<U256>,
-    Le<H::BlockSize, U256>: NonZero,
+    H: ExtendableOutputReset,
 {
-    fn new(input_bytes: Option<&[u8]>) -> Self {
-        let mut hasher: CoreWrapper<H> = Default::default();
-        if let Some(initial_data) = input_bytes {
-            hasher.update(initial_data);
+    fn absorb(&mut self, data: &[u8]) -> PyResult<()> {
+        Ok(self.0.update(data))
+    }
+
+    fn finalize(&mut self) -> PyResult<Sponge<H>> {
+        Ok(Sponge(self.0.finalize_xof_reset()))
+    }
+}
+
+struct Sponge<H: ExtendableOutputReset>(H::Reader);
+
+impl<H: ExtendableOutputReset> Sponge<H> {
+    fn read_into(&mut self, out: &mut [u8]) -> PyResult<()> {
+        Ok(self.0.read(out))
+    }
+}
+
+macro_rules! turboshake_create {
+    ($name:ty, $domain_sep:ident | $core:ident) => {
+        if (0x01..=0x7F).contains(&$domain_sep) {
+            Ok(Shaker(<$name>::from_core($core::new($domain_sep))))
+        } else {
+            Err(PyValueError::new_err(
+                "Domain separator not in range(1, 0x80)",
+            ))
         }
-        Self { hasher }
-    }
-
-    fn update(&mut self, data: &[u8]) {
-        self.hasher.update(data);
-    }
-
-    fn finalize_xof_reset(&mut self) -> Sponge<<CoreWrapper<H> as ExtendableOutput>::Reader> {
-        Sponge {
-            xof: self.hasher.finalize_xof_reset(),
-        }
-    }
+    };
 }
 
-struct Sponge<R: XofReader> {
-    xof: R,
-}
-
-impl<R: XofReader> Sponge<R> {
-    fn read(&mut self, out: &mut [u8]) {
-        self.xof.read(out);
-    }
-}
+impl_shaker_and_sponge!(Shaker128, Sponge128, Shake128, default);
+impl_shaker_and_sponge!(Shaker256, Sponge256, Shake256, default);
+impl_shaker_and_sponge!(
+    TurboShaker128,
+    TurboSponge128,
+    TurboShake128,
+    domain_sep: u8 => turboshake_create | TurboShake128Core
+);
+impl_shaker_and_sponge!(
+    TurboShaker256,
+    TurboSponge256,
+    TurboShake256,
+    domain_sep: u8 => turboshake_create | TurboShake256Core
+);
 
 /// A Python module implemented in Rust.
 #[pymodule]
-fn xof(m: &Bound<'_, PyModule>) -> PyResult<()> {
+fn xof_py(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_class::<Sponge128>()?;
     m.add_class::<Shaker128>()?;
     m.add_class::<Sponge256>()?;
