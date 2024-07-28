e9143dba3cedd2bb2dd375a2238e3b25221ba329
Sam Leonard <samuelleonard35@gmail.com>
2024-07-28 12:30:48 +0100

    Add Turbo* classes to xof_py.pyi

--- xof_py.pyi
+++ xof_py.pyi
@@ -25,3 +25,31 @@ class Shaker256:
 class Sponge256:
     def read(self, n: int) -> bytes:
         ...
+
+class TurboShaker128:
+    def __init__(self, domain_sep: int, input_bytes: bytes | None = None):
+        ...
+
+    def absorb(self, input_bytes: bytes) -> None:
+        ...
+
+    def finalize(self) -> Sponge128:
+        ...
+
+class TurboSponge128:
+    def read(self, n: int) -> bytes:
+        ...
+
+class TurboShaker256:
+    def __init__(self, domain_sep: int, input_bytes: bytes | None = None):
+        ...
+
+    def absorb(self, input_bytes: bytes) -> None:
+        ...
+
+    def finalize(self) -> Sponge128:
+        ...
+
+class TurboSponge256:
+    def read(self, n: int) -> bytes:
+        ...
