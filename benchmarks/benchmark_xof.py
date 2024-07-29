23b4850d17070aada65a1854a12c836b385911e5
Giacomo Pope <giacomopope@gmail.com>
2024-07-29 10:34:01 +0100

    clean up tests and benchmarks

--- benchmarks/benchmark_xof.py
+++ benchmarks/benchmark_xof.py
@@ -2,41 +2,31 @@ import random
 from hashlib import shake_128
 from xof import Shaker128
 import time
-import os
 from Crypto.Hash.SHAKE128 import SHAKE128_XOF
-
-def test_single_read():
-    for _ in range(100):
-        absorb = os.urandom(32)
-        n = random.randint(1, 1000)
-        a = shake_128(absorb).digest(n)
-        b = Shaker128(absorb).finalize().read(n)
-        assert a == b
-
-def test_many_reads():
-    for _ in range(3):
-        absorb = os.urandom(32)
-        xof1 = Shaker128(absorb).finalize()
-        xof2 = Shaker128(absorb).finalize()
-        a = shake_128(absorb).digest(100_000)
-        b = b"".join(xof1.read(1) for _ in range(100_000))
-        c = xof2.read(100_000)
-        assert a == b == c
 
 random.seed(0)
 t0 = time.time()
 xof = Shaker128(b"123").finalize()
 for _ in range(10_000):
-    n = random.randint(1, 5000)
+    n = random.randint(1, 500)
     a = xof.read(n)
-print(f"10_000 calls with class pyo3: {time.time() - t0 }")
+print(f"10_000 calls with xof library: {time.time() - t0 }")
 
 random.seed(0)
 t0 = time.time()
 for _ in range(10_000):
-    n = random.randint(1, 5000)
+    n = random.randint(1, 500)
     a = shake_128(b"123").digest(n)
 print(f"10_000 calls with hashlib: {time.time() - t0 }")
+
+random.seed(0)
+t0 = time.time()
+xof = SHAKE128_XOF()
+xof.update(b"123")
+for _ in range(10_000):
+    n = random.randint(1, 500)
+    a = xof.read(n)
+print(f"10_000 calls with pycryptodome: {time.time() - t0 }")
 
 print("-" * 80)
 
---
+++ tests/test_xof.py
@@ -0,0 +1,26 @@
+from hashlib import shake_128, shake_256
+from xof import Shaker128, Shaker256
+import unittest
+
+
+class TestShakeHashlib(unittest.TestCase):
+    def hashlib_test_long_calls(self, Shake, shake_hashlib):
+        absorb_bytes = b"testing_shake_long"
+        for l in [1, 100, 1000, 2000, 5000, 1_000_000]:
+            xof = Shake(absorb_bytes).finalize()
+            self.assertEqual(shake_hashlib(absorb_bytes).digest(l), xof.read(l))
+
+    def hashlib_test_many_calls(self, Shake, shake_hashlib):
+        absorb_bytes = b"testing_shake_one_byte"
+        for l in [1, 100, 1000, 2000, 5000, 1_000_000]:
+            xof = Shake(absorb_bytes).finalize()
+            output = b"".join([xof.read(1) for _ in range(l)])
+            self.assertEqual(shake_hashlib(absorb_bytes).digest(l), output)
+
+    def test_hashlib_shake128(self):
+        self.hashlib_test_long_calls(Shaker128, shake_128)
+        self.hashlib_test_many_calls(Shaker128, shake_128)
+
+    def test_hashlib_shake256(self):
+        self.hashlib_test_long_calls(Shaker256, shake_256)
+        self.hashlib_test_many_calls(Shaker256, shake_256)
