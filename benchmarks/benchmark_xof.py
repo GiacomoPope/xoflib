fa45b38519baf01f44a00eb63633af07b258a55d
Giacomo Pope <giacomopope@gmail.com>
2024-07-29 10:35:03 +0100

    clean up tests and benchmarks

--- benchmarks/benchmark_xof.py
+++ benchmarks/benchmark_xof.py
@@ -34,7 +34,7 @@ t0 = time.time()
 xof = Shaker128(b"123").finalize()
 for _ in range(1_000_000):
     a = xof.read(1)
-print(f"1_000_000 single byte reads pyo3: {time.time() - t0 }")
+print(f"1_000_000 single byte reads with xof library: {time.time() - t0 }")
 
 t0 = time.time()
 xof = SHAKE128_XOF()
@@ -47,7 +47,7 @@ t0 = time.time()
 xof = Shaker128(b"123").finalize()
 for _ in range(1_000_000):
     a = xof.read(168)
-print(f"100_000 block reads pyo3: {time.time() - t0 }")
+print(f"100_000 block reads with xof library: {time.time() - t0 }")
 
 t0 = time.time()
 xof = SHAKE128_XOF()
