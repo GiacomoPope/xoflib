6b0587be970bd1a2ff008deb01fa07de731e1ed5
tritoke <tritoke@protonmail.com>
2024-07-29 13:55:22 +0100

    Update xoflib.pyi for new class names

--- xoflib.pyi
+++ xoflib.pyi
@@ -1,4 +1,4 @@
-class Shaker128:
+class Shake128:
     def __init__(self, input_bytes: bytes | None = None):
         ...
 
@@ -12,7 +12,7 @@ class Sponge128:
     def read(self, n: int) -> bytes:
         ...
 
-class Shaker256:
+class Shake256:
     def __init__(self, input_bytes: bytes | None = None):
         ...
 
@@ -26,7 +26,7 @@ class Sponge256:
     def read(self, n: int) -> bytes:
         ...
 
-class TurboShaker128:
+class TurboShake128:
     def __init__(self, domain_sep: int, input_bytes: bytes | None = None):
         ...
 
@@ -40,7 +40,7 @@ class TurboSponge128:
     def read(self, n: int) -> bytes:
         ...
 
-class TurboShaker256:
+class TurboShake256:
     def __init__(self, domain_sep: int, input_bytes: bytes | None = None):
         ...
 
